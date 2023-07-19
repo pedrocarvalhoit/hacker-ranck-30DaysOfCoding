740d8fa09b374414f4ea907d56f332a8ba95dd53
Pedro Duarte <pedrohdc@yahoo.com.br>
2023-07-19 20:39:20 +0100

    Intro to Conditional Statements

---
+++ src/main/java/weekOne/Day3.java
@@ -0,0 +1,36 @@
+package weekOne;
+
+import java.util.Random;
+
+/**Task
+ Given an integer, n, perform the following conditional actions:
+
+ If  is odd, print Weird
+ If  is even and in the inclusive range of 2 to 5, print Not Weird
+ If  is even and in the inclusive range of  6 to 20, print Weird
+ If  is even and greater than 20, print Not Weird
+ Complete the stub code provided in your editor to print whether or not n is weird. */
+
+public class Day3 {
+
+    public static void main(String[] args) {
+
+        Random random  = new Random();
+        int n = random.nextInt(30);
+        System.out.println(n);
+
+        if (n % 2 != 0 ){
+            System.out.println("Weird");
+        }else if (n % 2 == 0 && n >= 2 && n <= 5){
+            System.out.println("Not Weird");
+        }else if (n % 2 == 0 && n >= 6 && n <= 20) {
+            System.out.println("Weird");
+        } else if (n % 2 == 0 && n >20) {
+            System.out.println("Not Weird");
+        }
+
+    }
+
+}
+
+
