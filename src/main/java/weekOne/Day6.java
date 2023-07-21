4c766144168b0f2266be0a5c77371066ae9971ea
Pedro Duarte <pedrohdc@yahoo.com.br>
2023-07-21 20:41:05 +0100

    Loops and Split String

---
+++ src/main/java/weekOne/Day6.java
@@ -0,0 +1,54 @@
+package weekOne;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+/** Given a string, s, of length n that is indexed from 0 to n-1, print its even-indexed
+        and odd-indexed characters as  3space-separated strings on a single line
+        (see the Sample below for more detail).
+
+        Note:  0 is considered to be an even index.*/
+
+public class Day6 {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        Result result = new Result();
+
+        System.out.println("How many words do you wnat to split?");
+        int T = scan.nextInt();
+        scan.nextLine();
+        for (int i = 0; i<T; i++){
+            System.out.println("Write a String to split on evens and odds");
+            String s = scan.nextLine();
+            result.spliter(s);
+        }
+        scan.close();
+
+    }
+
+    public static class Result{
+        public void spliter(String s){
+
+           char[] charArray = s.toCharArray();
+
+           StringBuilder evenIndexesString = new StringBuilder();
+           StringBuilder oddIndexesString = new StringBuilder();
+
+           for (int i = 0; i < charArray.length; i++){
+               if (i % 2 == 0){
+                   evenIndexesString.append(charArray[i]);
+               }else{
+                   oddIndexesString.append(charArray[i]);
+               }
+           }
+
+           String evenIndexesResult = evenIndexesString.toString();
+           String oddIndexesResult = oddIndexesString.toString();
+
+            System.out.println(evenIndexesResult + " " + oddIndexesResult);
+        }
+    }
+
+}
