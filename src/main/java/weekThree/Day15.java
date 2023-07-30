b2c2fef0e073d6dc718edc7e96a7033322b4ed3f
Pedro Duarte <pedrohdc@yahoo.com.br>
2023-07-30 11:51:51 +0100

    Linked List

---
+++ src/main/java/weekThree/Day15.java
@@ -0,0 +1,56 @@
+package weekThree;
+
+// https://www.hackerrank.com/challenges/30-linked-list/problem?h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Day15 {
+
+    static class Node {
+        int data;
+        Node next;
+
+        public Node() {
+        }
+
+        Node(int d) {
+            data = d;
+            next = null;
+        }
+
+        public Node insert(Node head, int data){
+            if(head==null){
+                return new Node(data);
+            }
+            head.next = insert(head.next, data);
+            return head;
+        }
+
+        public void display(Node head){
+            Node start = head;
+            while(start != null){
+                System.out.print(start.data + " ");
+                start = start.next;
+            }
+        }
+
+    }
+
+    public static void main(String[] args) {
+        Node head = new Node();
+        List<Integer> list = new ArrayList<>();
+        list.add(4);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.add(1);
+
+        for(Integer number:list){
+            head.insert(head, number);
+        }
+        head.display(head);
+
+    }
+
+}
