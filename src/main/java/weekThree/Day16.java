3fd56d752f9cb1a4e29cc9796eba2f296f3b900d
Pedro Duarte <pedrohdc@yahoo.com.br>
2023-07-31 14:02:37 +0100

    Exception

---
+++ src/main/java/weekThree/Day16.java
@@ -0,0 +1,25 @@
+package weekThree;
+
+//https://www.hackerrank.com/challenges/30-exceptions-string-to-integer/problem?h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen
+
+import java.util.Scanner;
+
+public class Day16 {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Give me a String, with just numbers.");
+        String string = scan.nextLine();
+
+        try {
+            int number = Integer.parseInt(string);
+            System.out.println(number);
+        } catch (NumberFormatException e) {
+            System.out.println("Bad String");
+        }
+
+
+    }
+
+}
