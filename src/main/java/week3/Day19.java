58b05e688e8f1e082e3c8186a5cebdca67894319
Pedro Duarte <pedrohdc@yahoo.com.br>
2023-08-02 15:04:58 +0100

    Interfaces

--- src/main/java/week3/Day19.java
+++ src/main/java/week3/Day19.java
@@ -1,4 +1,4 @@
-package weekThree;
+package week3;
 
 //https://www.hackerrank.com/challenges/30-interfaces/problem?h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen
 
